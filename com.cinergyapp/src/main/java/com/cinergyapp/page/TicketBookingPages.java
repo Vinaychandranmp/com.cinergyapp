651294c002675c9cc38f716fdf86e571c4435fe7
vinay <vinay@DESKTOP-MUJMI6S>
2024-01-14 23:13:52 +0530

    Third commit

--- com.cinergyapp/src/main/java/com/cinergyapp/page/TicketBookingPages.java
+++ com.cinergyapp/src/main/java/com/cinergyapp/page/TicketBookingPages.java
@@ -1,15 +1,23 @@
 package com.cinergyapp.page;
 
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.FindBy;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TicketBookingPages 
 {
 	public WebDriver driver;
+	Properties properties=new Properties();
+	@FindBy(xpath="//select[@id='location_select']//option[contains(text(),'Charlotte, NC')]")
+	WebElement location_charlotte;
+	
 	public void initialize(String browser, String url) 
 	{
 		if (browser.equals("chrome")) 
@@ -25,7 +33,11 @@ public class TicketBookingPages
 			WebDriverManager.firefoxdriver().setup();
 			driver = new FirefoxDriver();
 		}
+		driver.get(url);
 	
 	}
-	
+	public void select_location()
+	{
+		location_charlotte.click();
+	}
 }
--- com.cinergyapp/src/test/java/com/cinergyapp/test/TicketBookingTest.java
+++ com.cinergyapp/src/test/java/com/cinergyapp/test/TicketBookingTest.java
@@ -1,5 +1,22 @@
 package com.cinergyapp.test;
 
-public class TicketBookingTest {
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
+import com.cinergyapp.page.TicketBookingPages;
+
+public class TicketBookingTest 
+{	
+	TicketBookingPages ticketBookingPage=new TicketBookingPages();
+	@BeforeTest
+	public void launch_url()
+	{
+		ticketBookingPage.initialize("chrome", "https://cinergyapp.thetunagroup.com/");
+	}
+	@Test
+	public void movieTicketBooking()
+	{
+		ticketBookingPage.select_location();
+	}
 }
+
